21aef6bcaa7730697fcff0901e7fed3f52e97656
guruprasadDamodare <97739579+guruprasadDamodare@users.noreply.github.com>
2022-04-11 16:55:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var images=["https://s3-projectsassets.whjr.online/Monkey_01.png","https://s3-projectsassets.whjr.online/Monkey_02.png",
+"https://s3-projectsassets.whjr.online/Monkey_03.png","https://s3-projectsassets.whjr.online/Monkey_04.png"
+,"https://s3-projectsassets.whjr.online/Monkey_05.png","https://s3-projectsassets.whjr.online/Monkey_06.png","https://s3-projectsassets.whjr.online/Monkey_07.png"
+,"https://s3-projectsassets.whjr.online/Monkey_08.png","https://s3-projectsassets.whjr.online/Monkey_09.png","https://s3-projectsassets.whjr.online/Monkey_10.png"]
+var i=0
+function move(){
+
+document.getElementById("monkey").src=images[i]
+i++
+
+}
+
+
+
+
+
